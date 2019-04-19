99fb37a8fb5f9baeaf678e0ce206ce6566ca632c
Matt Boyas <mboyas@mitre.org>
2019-04-19 11:29:07 -0400

    added test files

---
+++ Tariff/tests/testthat/tests.Tariff.R
@@ -0,0 +1,19 @@
+context("Tariff Algorithm")
+
+data("RandomVA3")
+test <- RandomVA3[1:200, ]
+train <- RandomVA3[201:400, ]
+
+test_that("Training data all has the same cause", {
+
+  train$cause <- rep("Pulmonary TB", nrow(train))
+
+  allcauses <- unique(train$cause)
+
+
+  fit <- tariff(causes.train = "cause", symps.train = train,
+        symps.test = test, causes.table = allcauses)
+
+  expect_equal("Pulmonary TB", unique(as.character(fit$causes.test[,2])))
+
+})
